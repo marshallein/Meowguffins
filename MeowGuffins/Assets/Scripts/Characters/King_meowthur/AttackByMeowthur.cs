686956d2e553b0f4bcf68ee6525360bafec63ad8
Hanh Nguyen <letsmailhanh@gmail.com>
2022-02-10 23:23:12 +0700

    add meowthur attack

---
+++ MeowGuffins/Assets/Scripts/Characters/King_meowthur/AttackByMeowthur.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class AttackByMeowthur : MonoBehaviour
+{
+    private Animator animator;
+    Rigidbody rb;
+    float nextAttackTime = 0f;
+    float attackRate = 2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void attack_timmer(string trigger)
+    {
+        if (Time.time >= nextAttackTime)
+        {
+            animator.SetTrigger(trigger);
+            nextAttackTime = Time.time + 1f / attackRate;
+        }
+    }
+    public void OnCharacterAttack(InputAction.CallbackContext context)
+    {
+        attack_timmer("attack_set");
+    }
+
+    public void OnCharacterAttack2(InputAction.CallbackContext context)
+    {
+        attack_timmer("attack_set2");
+    }
+
+    public void OnCharacterAttack3(InputAction.CallbackContext context)
+    {
+        attack_timmer("attack_set3");
+    }
+    public void OnCharacterAttack4(InputAction.CallbackContext context)
+    {
+        attack_timmer("attack_set4");
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        print("Hit " + collision.name);
+    }
+}
