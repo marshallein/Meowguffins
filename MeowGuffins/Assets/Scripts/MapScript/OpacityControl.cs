2ea65abac1eff9e53cdc231947cb9b55de892ce8
marshallein <maxboiz123hn@gmail.com>
2022-02-20 19:06:55 +0700

    [Add] Add map Jungle

---
+++ MeowGuffins/Assets/Scripts/MapScript/OpacityControl.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class OpacityControl : MonoBehaviour
+{
+    Tilemap m_tileMap;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_tileMap = GetComponent<Tilemap>();
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            Debug.Log("player enter the leaf");
+            m_tileMap.color = new Color(1f, 1f, 1f, 0.4f);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        m_tileMap.color = new Color(1f, 1f, 1f, 1f);
+    }
+}
