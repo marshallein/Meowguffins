454f749b531b8309f31b450a6951dc2a8b496d99
hn7470151@gmail.com <61149257+hoanghoang1910@users.noreply.github.com>
2022-02-23 23:30:59 +0700

    adda health for characters and demo AI enamy
    adda health for characters and demo AI enamy

---
+++ MeowGuffins/Assets/Scripts/Characters/Others/HealthBar.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Image hpImage;
+    public Image hpEffectImage;
+    public float hp;
+    [SerializeField]
+    private float maxHp;
+    [SerializeField]
+    private float hurtSpeed = 0.005f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        hp = maxHp;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hpImage.fillAmount = hp / maxHp;
+
+        if(hpEffectImage.fillAmount > hpImage.fillAmount)
+        {
+            hpEffectImage.fillAmount -= hurtSpeed;
+        }
+        else
+        {
+            hpEffectImage.fillAmount = hpImage.fillAmount;
+        }
+    }
+}
