84027983b897d1a84580b41becf41a586ff08fea
letsmailhanh <letsmailhanh@gmail.com>
2022-03-10 08:11:19 +0700

    null check before using left/rightTarget & target in EnemyMeleeBehaviour

--- MeowGuffins/Assets/Scripts/Enemy/MeleeEnemy/HotZoneCheckScript.cs
+++ MeowGuffins/Assets/Scripts/Enemy/MeleeEnemy/HotZoneCheckScript.cs
@@ -7,6 +7,7 @@ public class HotZoneCheckScript : MonoBehaviour
     private EnenyMeleeBehaviour m_parent;
     private bool inRange;
     private Animator animator;
+    private GameObject target;
 
 
     private void Awake()
@@ -19,14 +20,17 @@ public class HotZoneCheckScript : MonoBehaviour
     {
         if (inRange && !animator.GetCurrentAnimatorStateInfo(0).IsName("isAttack"))
         {
+            m_parent.target = target.transform;
             m_parent.OnFlip();
         }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        var target = collision.gameObject;
+        if (target.CompareTag("Player"))
         {
+            this.target = target;
             inRange = true;
         }
     }
@@ -40,8 +44,8 @@ public class HotZoneCheckScript : MonoBehaviour
             gameObject.SetActive(false);
             m_parent.triggerZone.SetActive(true);
             m_parent.inRange = false;
-            m_parent.OnSelectAttack(); 
-
+            m_parent.OnSelectAttack();
+            this.target = null;
         }
     }
 }
